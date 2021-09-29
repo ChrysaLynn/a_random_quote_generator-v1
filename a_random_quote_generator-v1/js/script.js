3eaefdf2f76a190bb682ef09c5e5f5e9cdb06c36
ChrysaLynn <chrysalis.parker@gmail.com>
2021-09-29 16:11:25 -0700

    Completed JS and updated README

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,31 +3,87 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// Random quotes array 
 
-/*** 
- * `quotes` array 
-***/
+const quotes = [
+  {
+    quote: 'Words offer the means to meaning, and for those who will listen, the enunciaiton of truth.',
+    source: 'V',
+    citation: 'V for Vendetta',
+    year: '2005'
+  }, 
+  {
+   quote: 'If you are working on something you really care about, you don\'t have to be pushed. The vision pulls you.',
+   source: 'Steve Jobs',
+   citation: '',
+   year: ''
+  },
+  {
+    quote: 'Women challenge the status quo because we are never it.',
+    source: 'Cindy Gallop',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'The greatest discovery of my generation is that a human being can alter his life by altering his attitudes.',
+    source: 'William James',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'The most dangerous phrase in the language is, "We\'ve always done it this way."',
+    source: 'Grace Hopper',
+    citation: '',
+    year: ''
+  },
+  {
+    quote: 'The one thing that doesn\'t abide by majority rule is a person\'s conscience.',
+    source: 'Atticus Finch',
+    citation: 'To Kill a Mockingbird (by Harper Lee)',
+    year: '1960'
+  },
+  {
+    quote: 'I never look back darling, it distracts from the now. ',
+    source: 'Edna Mode',
+    citation: 'The Incredibles',
+    year: '2004'
+  },
+  {
+    quote: 'We build our computer (systems) the way we build our citites: over time, without a plan, on top of ruins.',
+    source: 'Ellen Ullman',
+    citation: 'Twitter @CodeWisdom',
+    year: '2017'
+  } 
+]
 
+// Function to create a random number to retrieve an random array item
 
+function getRandomQuote () {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  return randomNumber;
+} 
 
-/***
- * `getRandomQuote` function
-***/
+// Declaring the variables used in the function below
 
+let quoteString;
+let quoteObject;
 
+// This function will call the getRandomQuote() function, then use that array item to create an HTML string with quote properties that'll be displayed in the browser
 
-/***
- * `printQuote` function
-***/
+function printQuote () {
+  quoteObject = quotes[getRandomQuote()];
+  quoteString = `<p class="quote">${quoteObject.quote}</p>
+  <p class="source">${quoteObject.source}`;
+  if (quoteObject.citation) {
+    quoteString += `<span class="citation">${quoteObject.citation}</span>`;
+  }
+  if (quoteObject.year) {
+    quoteString += `<span class="year">${quoteObject.year}</span>`;
+  }
+  quoteString += `</p>`;
+  console.log(quoteObject);
+  document.getElementById('quote-box').innerHTML = quoteString;
+}
 
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
